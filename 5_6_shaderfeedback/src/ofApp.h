b4cb803d12f9bb4c1ece5406c43af25433c930c0
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2023-01-27 15:01:45 +0900

    add gui,format

--- 5_6_shaderfeedback/src/ofApp.h
+++ 5_6_shaderfeedback/src/ofApp.h
@@ -1,54 +1,52 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 #include "ofxOsc.h"
 
-enum ControlMode { AUTO=0, MANUAL=1, OSC=2 };
+enum ControlMode { AUTO = 0, MANUAL = 1, OSC = 2 };
 
-enum MessageType {
-  BLUR_AMOUNT,
-  SCALE,
-  UNKNOWN
-};
-MessageType addressToMessage(std::string const& str);
+enum MessageType { BLUR_AMOUNT, SCALE, UNKNOWN };
+MessageType addressToMessage(std::string const &str);
 
 class ofApp : public ofBaseApp {
 
-public:
-  void setup();
-  void update();
-  void draw();
+  public:
+    void setup();
+    void update();
+    void draw();
 
-  void keyPressed(int key);
-  void keyReleased(int key);
-  void mouseMoved(int x, int y);
-  void mouseDragged(int x, int y, int button);
-  void mousePressed(int x, int y, int button);
-  void mouseReleased(int x, int y, int button);
-  void mouseEntered(int x, int y);
-  void mouseExited(int x, int y);
-  void windowResized(int w, int h);
-  void dragEvent(ofDragInfo dragInfo);
-  void gotMessage(ofMessage msg);
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
 
-private:
-  void resetBlank();
-  void resetCircle();
-  static void applyBlur1d(ofShader &blurshader, ofFbo &target, float amount);
-  void applyBlur(ofFbo &target, float amount);
-  void updateParams();
-  void drawMainShader();
-  void processOscMessage();
+  private:
+    void resetBlank();
+    void resetCircle();
+    static void applyBlur1d(ofShader &blurshader, ofFbo &target, float amount);
+    void applyBlur(ofFbo &target, float amount);
+    void updateParams();
+    void drawMainShader();
+    void processOscMessage();
 
-  ofShader shader;
-  ofShader noise;
-  ofShader blurx, blury, centercircle;
-  ofFbo fbo1;
-  ofxOscReceiver osc;
-  ofxOscMessage latest_msg;
-  float time;
-  float myscale;
-  float myblur;
-  ControlMode mode;
-  bool debug_view;
+    ofShader shader;
+    ofShader noise;
+    ofShader blurx, blury, centercircle;
+    ofFbo fbo1;
+    ofxOscReceiver osc;
+    ofxOscMessage latest_msg;
+	
+    float time;
+    float myscale;
+    float myblur;
+    ControlMode mode;
+    bool debug_view;
 };
